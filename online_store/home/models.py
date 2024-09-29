77dfbad8d0f75c1b198020bd081346b2e0a8006c
ahmed <ahmedemad2040outlook@gmail.com>
2024-09-29 14:12:05 +0300

    changes99

--- online_store/home/models.py
+++ online_store/home/models.py
@@ -9,20 +9,12 @@ class Product(models.Model):
     desc = models.TextField(blank=True, null=True)
     sku = models.CharField(db_column='SKU', blank=True, null=True, max_length=255) 
     category = models.ForeignKey('ProductCategory', models.DO_NOTHING, blank=True, null=True)
-    inventory = models.ForeignKey('ProductInventory', models.DO_NOTHING, blank=True, null=True)
+    inventory_quantity = models.IntegerField(blank=True, null=True)  # New field for inventory quantity
     price = models.DecimalField(max_digits=10, decimal_places=1, blank=True, null=True)
     reviews_count = models.CharField(blank=True, null=True, max_length=255)
     created_at = models.DateField(default=now)
     modified_at = models.DateField(blank=True, null=True)  
-    deleted_at = models.DateField(blank=True, null=True)  
-
-
-
-class ProductInventory(models.Model):
-    quantity = models.IntegerField(blank=True, null=True)
-    created_at = models.DateField(default=date.today)  
-    modified_at = models.DateField(blank=True, null=True)  
-    deleted_at = models.DateField(blank=True, null=True)  
+    deleted_at = models.DateField(blank=True, null=True) 
 
 
 
--- online_store/online_store/settings.py
+++ online_store/online_store/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "online_store",
         "USER": "postgres",
-        "PASSWORD": "kali",
+        "PASSWORD": "ahmed3omda",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
